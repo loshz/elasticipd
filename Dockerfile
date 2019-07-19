7b499703bd4a6c96945627095ee4217183e1fc75
Dan Bond <pm@danbond.io>
2019-07-19 13:27:47 +0100

    fix docker push

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:alpine
+FROM golang:1.12-alpine
 WORKDIR /go/src/github.com/syscll/elasticipd
 COPY . .
 RUN go install
@@ -6,9 +6,5 @@ RUN go install
 FROM alpine
 # Manually add dependencies
 RUN apk add --no-cache ca-certificates
-RUN apk add --no-cache python3
-# Install awscli
-RUN pip3 install --upgrade pip && \
-    pip3 install awscli
 # Copy binary from build container
 COPY --from=0 /go/bin/elasticipd  /usr/local/bin
--- docker.sh
+++ docker.sh
@@ -1,14 +1,8 @@
 #! /usr/bin/env bash
 
-function error {
-	echo ERROR: "$1"
-	exit 1
-}
-
-command -v docker >/dev/null 2>&1 || error "docker not installed"
+set -o errexit
 
 DOCKER_IMAGE="syscll/elasticipd:latest"
 
-docker build -t $DOCKER_IMAGE . || error "failed to build Docker image"
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker push $DOCKER_IMAGE || error "failed to distribute Docker image"
+docker build --tag $DOCKER_IMAGE .
+docker push $DOCKER_IMAGE
